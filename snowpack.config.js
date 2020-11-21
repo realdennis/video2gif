8237cd753205443aa45c4703019b2a61af713845
Dennis <realdennis777@gmail.com>
2020-11-21 23:28:37 +0800

    dev: using snowpack instead of parcel, test the web import modules power (#10)
    * dev: using snowpack instead of parcel, test the web import modules power
    
    * dev: align the build target dir as master branch

---
+++ snowpack.config.js
@@ -0,0 +1,30 @@
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  mount: {
+    public: "/",
+    src: "/_dist_",
+  },
+  plugins: [
+    ["@snowpack/plugin-sass"],
+    /* ... */
+  ],
+  install: [
+    /* ... */
+  ],
+  installOptions: {
+    /* ... */
+  },
+  devOptions: {
+    /* ... */
+  },
+  buildOptions: {
+    out: "dist",
+    /* ... */
+  },
+  proxy: {
+    /* ... */
+  },
+  alias: {
+    /* ... */
+  },
+};
