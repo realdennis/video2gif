e08d5502893b82e23644861234bf60390e329200
realdennis <realdennis777@gmail.com>
2020-11-21 23:18:08 +0800

    dev: using snowpack instead of parcel, test the web import modules power

---
+++ snowpack.config.js
@@ -0,0 +1,29 @@
+/** @type {import("snowpack").SnowpackUserConfig } */
+module.exports = {
+  mount: {
+    public: '/',
+    src: '/_dist_',
+  },
+  plugins: [
+    ['@snowpack/plugin-sass'],
+    /* ... */
+  ],
+  install: [
+    /* ... */
+  ],
+  installOptions: {
+    /* ... */
+  },
+  devOptions: {
+    /* ... */
+  },
+  buildOptions: {
+    /* ... */
+  },
+  proxy: {
+    /* ... */
+  },
+  alias: {
+    /* ... */
+  },
+};
